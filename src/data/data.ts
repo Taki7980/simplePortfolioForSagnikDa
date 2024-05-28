d6627faea25dca2095016a99501816d3706f2785
nirjhar sarkar <nirjharsarkar8@gmail.com>
2024-05-28 21:05:31 +0530

    starter

---
+++ src/data/data.ts
@@ -0,0 +1,20 @@
+const ProjectData=[
+      // replicate this to add new field
+      {
+            title:"",
+            subtitle:"",
+            date:"",
+            description:"",
+            image:"",
+            subsection:[
+                  // replicate this to add new subsection
+                  {
+                        title:"",
+                        description:"",
+                        date:"",
+                        image:"",
+                        link:"",
+                  }
+            ]
+      },
+]
