7e1df3290c9323176a4b77c55c5e2cbbc612a519
nirjhar sarkar <nirjharsarkar8@gmail.com>
2024-05-30 17:57:13 +0530

    reverted the card and wrok/id route

---
+++ src/app/work/[slug]/page.tsx
@@ -0,0 +1,32 @@
+import Heading from "@/components/ui/heading";
+import SubCard from "@/components/ui/subCard";
+import ProjectData from "@/data/data";
+
+import React from "react";
+
+const page = async ({ params }: { params: { slug: string } }) => {
+	const init = ProjectData.find((item) => item.id === params.slug);
+	const details = ProjectData.find(
+		(item) => item.id === params.slug
+	)?.subsection;
+
+	return (
+		<div className="max-w-6xl h-full flex flex-col items-center justify-center mx-auto mt-[-80px] pt-10 bg-white rounded-lg pb-20">
+			<Heading title={init?.title || "welcome"} center />
+			<div className=" grid grid-cols-1 md:grid-cols-3 pt-10 gap-4 justify-center w-full px-10">
+				{details?.map((item, index) => (
+					<SubCard
+						key={index}
+						title={item.title}
+						description={item.description}
+						date={item.date}
+						image={item.image}
+						link={item.link}
+					/>
+				))}
+			</div>
+		</div>
+	);
+};
+
+export default page;
--- src/data/data.ts
+++ src/data/data.ts
@@ -7,23 +7,29 @@ const ProjectData = [
 
 		description: `India's air quality remains a pressing concern. In 2021, the World Health Organization (WHO) revised its air quality guidelines, making them stricter. These revisions came in the wake of growing scientific evidence about the damaging effects of air pollution. In fact, the new WHO Air Quality Guidelines (AQGs), following its last global update in 2005, are aimed at providing a clearer framework for countries to combat and reduce the adverse health impacts of air pollution.`,
 		image: "/images/climate.png",
-
+		date: "20/5/2024",
 		subsection: [
 			// replicate this to add new subsection
 			{
 				title: "Watch it on YouTube",
 				image: "/images/cloud5.jpg",
 				link: "https://www.youtube.com/watch?v=HlGRAGJrCcE&ab_channel=TheProbe",
+				description: "Watch it on YouTube",
+				date: "20/5/2024",
 			},
 			{
 				title: "ThePhobe report",
 				image: "/images/cloud5.jpg",
 				link: "https://theprobe.in/stories/indias-air-quality-crisis-why-isnt-the-cpcb-revising-its-air-quality-standards/",
+				description: "Watch it on YouTube",
+				date: "20/5/2024",
 			},
 			{
 				title: "Another Report on youtube",
 				image: "/images/cloud5.jpg",
 				link: "https://www.youtube.com/watch?v=VwGC-mJS2ik&t=2s",
+				description: "Watch it on YouTube",
+				date: "20/5/2024",
 			},
 		],
 	},
@@ -31,78 +37,94 @@ const ProjectData = [
 		// Id have to follow the order for fetchting the data
 		id: "2",
 		title: "women centric reporting",
-
 		description: `Women-centric reporting in journalism in India has gained significant momentum in recent years, reflecting a broader societal shift towards gender equality and women's empowerment. This form of journalism prioritizes issues that predominantly affect women, such as gender-based violence, health, education, economic empowerment, and representation in various spheres. Media outlets have increasingly recognized the importance of providing a platform for women's voices, addressing their concerns, and challenging stereotypes. `,
 		image: "/images/women.png",
-
+		date: "20/5/2024",
 		subsection: [
 			// replicate this to add new subsection
 			{
 				title: "ThePhobe report",
+				image: "/images/cloud5.jpg",
 				link: "https://theprobe.in/stories/stripped-paraded-assaulted-the-story-of-lali-devi/",
+				description: "Watch it on YouTube",
+				date: "20/5/2024",
 			},
 			{
 				title: "ThePhobe report",
+				image: "/images/cloud5.jpg",
 				link: "https://theprobe.in/public-interest/womens-commission-across-many-states-in-shocking-turmoil-4492151",
+				description: "Watch it on YouTube",
+				date: "20/5/2024",
 			},
 			{
 				title: "ThePhobe report",
+				image: "/images/cloud5.jpg",
 				link: "https://theprobe.in/stories/confronting-gender-inequality-and-pay-gap-in-indias-workforce/",
+				description: "Watch it on YouTube",
+				date: "20/5/2024",
 			},
 		],
 	},
-      {
+	{
 		// Id have to follow the order for fetchting the data
 		id: "3",
 		title: "Finance",
 		description: `In the dynamic world of finance, cryptocurrencies have emerged as a significant innovation, challenging the conventional monetary systems. These digital or virtual currencies, built on advanced cryptographic technology, operate independently of traditional central banking systems. `,
 		image: "/images/finance.png",
-
+		date: "20/5/2024",
 		subsection: [
 			// replicate this to add new subsection
 			{
 				title: "ThePhobe report",
+				image: "/images/cloud5.jpg",
 				link: "https://theprobe.in/stories/cryptocurrency-regulatory-uncertainty-and-economic-implications/",
-			}
-			
+				description: "Watch it on YouTube",
+				date: "20/5/2024",
+			},
 		],
 	},
 	{
 		// Id have to follow the order for fetchting the data
 		id: "4",
 		title: "Healthcare reporting",
-		description: `Healthcare reporting in journalism in India plays a crucial role in informing the public about health issues, medical advancements, and policy changes. Journalists covering this beat often delve into topics ranging from public health crises, such as the COVID-19 pandemic, to endemic issues like malnutrition and infectious diseases. They are tasked with scrutinizing government health policies, highlighting gaps in healthcare infrastructure, and exposing instances of medical negligence and corruption. In a country with vast disparities in healthcare access and quality, responsible reporting can drive public awareness and policy reform.`,
+		description: `Healthcare reporting in journalism in India plays a crucial role in informing the public about health issues, medical advancements, and policy changes. Journalists covering this beat often delve into topics ranging from public health crises, such as the COVID-19 pandemic, to endemic issues like malnutrition and infectious diseases. They are tasked with scrutinizing government health policies`,
 		image: "/images/health.png",
-
+		date: "20/5/2024",
 		subsection: [
 			// replicate this to add new subsection
 			{
 				title: "ThePhobe report",
+				image: "/images/cloud5.jpg",
 				link: "https://theprobe.in/stories/polio-vaccine-derived-virus-whos-controversial-stance-and-indias-silence/",
-			}
-			
+				description: "Watch it on YouTube",
+				date: "20/5/2024",
+			},
 		],
 	},
 	{
 		// Id have to follow the order for fetchting the data
 		id: "5",
 		title: "Sports",
-		description: `Healthcare reporting in journalism in India plays a crucial role in informing the public about health issues, medical advancements, and policy changes. Journalists covering this beat often delve into topics ranging from public health crises, such as the COVID-19 pandemic, to endemic issues like malnutrition and infectious diseases. They are tasked with scrutinizing government health policies, highlighting gaps in healthcare infrastructure, and exposing instances of medical negligence and corruption. In a country with vast disparities in healthcare access and quality, responsible reporting can drive public awareness and policy reform.`,
+		description: `Healthcare reporting in journalism in India plays a crucial role in informing the public about health issues, medical advancements, and policy changes. Journalists covering this beat often delve into topics `,
 		image: "/images/sports.png",
-
+		date: "20/5/2024",
 		subsection: [
 			// replicate this to add new subsection
 			{
 				title: "ThePhobe report",
+				image: "/images/cloud5.jpg",
 				link: "https://theprobe.in/stories/doping-in-india-a-looming-crisis-threatening-fair-play/",
+				description: "Watch it on YouTube",
+				date: "20/5/2024",
 			},
 			{
 				title: "essentiallysports.com report",
+				image: "/images/cloud5.jpg",
 				link: "https://www.essentiallysports.com/nfl-news-moved-by-nine-eleven-misfortune-former-arizona-cardinal-laid-down-his-nfl-career-to-serve-higher-calling/",
-			}
-			
+				description: "Watch it on YouTube",
+				date: "20/5/2024",
+			},
 		],
 	},
-	
 ];
 export default ProjectData;
